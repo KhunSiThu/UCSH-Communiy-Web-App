29303f6c41b6bc67806d22fc6bd04408f32cdabf
KhunSiThu <khunsithuaung65@gmail.com>
2025-03-05 14:29:18 +0630

    delete group ui done

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 
-php -S 192.168.90.132:9000
-# php -S localhost:9000
+# php -S 192.168.90.132:9000
+php -S localhost:9000
 # php -S 172.30.1.0:9000
--- src/JS/function.js
+++ src/JS/function.js
@@ -648,7 +648,7 @@ const groupChat = async (chooseId) => {
                                 <span class="bottom-0 left-4 absolute w-2 h-2 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full transition-opacity duration-200 ${member.status !== 'Online' ? 'opacity-0' : 'opacity-100'}"></span>
                             </div>
                             
-                            <div onclick="openProfile(${friend.member})" class="flex cursor-pointer w-full ml-2 justify-between items-center">
+                            <div onclick="openProfile(${member.userId})" class="flex cursor-pointer w-full ml-2 justify-between items-center">
                                 <h2 class='text-sm'>${member.name}</h2> <span class="text-green-500  text-xs">${isAdmin}</span>
                             </div>
                         </div>
@@ -734,12 +734,20 @@ const groupChat = async (chooseId) => {
                                 <span>Add Member</span>
                             </button>
                         </li>
-                        <li>
+                        <li class="${group.adminId != userId ? 'block' : 'hidden' } ">
                             <button id="openLeaveGroupModal" class="hover:bg-gray-200 dark:hover:bg-gray-800">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
                                 </svg>
                                 <span>Leave Group</span>
+                            </button>
+                        </li>
+                        <li class="${group.adminId === userId ? 'block' : 'hidden'} ">
+                            <button id="" class="hover:bg-gray-200 dark:hover:bg-gray-800">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                </svg>
+                                <span>Delete Group</span>
                             </button>
                         </li>
                     </ul>
@@ -1120,6 +1128,7 @@ const groupChat = async (chooseId) => {
         location.reload();
     });
 };
+
 
 function openImageModal(imageSrc) {
     const modal = `
